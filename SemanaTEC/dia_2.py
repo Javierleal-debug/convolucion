96a9a3341218bf89bb048a7f229ac951b02fc938
Javierleal-debug <67740967+Javierleal-debug@users.noreply.github.com>
2020-09-15 12:43:20 -0600

    Update dia_2.py
    Idk porque el código no se habia subido

--- SemanaTEC/dia_2.py
+++ SemanaTEC/dia_2.py
@@ -1,1 +1,13 @@
 dia_2.py
+
+import cv2
+import numpy as np
+
+img = "perro.png"
+im = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+a = [ [ -1.0, 0.0, 1.0 ],
+      [ -1.0, 0.0, 1.0 ],
+      [ -1.0, 0.0, 1.0 ] ]
+kernel = np.asarray(a)
+dst = cv2.filter2D(im, -1, kernel)
+print(dst.min(), dst.max())
